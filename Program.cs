db5949d898acbfee9f8f93dacb0b4c3b1154c7a5
HP <HP@DESKTOP-6SUN5DR>
2019-05-21 11:05:18 +1200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace @switch
+    // the program is used the switch statement
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter any char");
+            char ch = char.Parse(Console.ReadLine());
+            String st = ch.ToString().ToLower();
+            switch(st)
+            {
+             case "a":
+            Console.WriteLine(st + "is a vowel");
+                    break;
+                case "i":
+                    Console.WriteLine(st + "is a vowel");
+                    break;
+                case "e":
+                    Console.WriteLine(st + "is a vowel");
+                    break;
+                case "u":
+                    Console.WriteLine(st + "is a vowel");
+                    break;
+                case "o":
+                    Console.WriteLine(st + "is a vowel");
+                    break;
+                default:
+                    Console.WriteLine(st + "is not a vowel");
+                    break;
+            }
+                
+        }
+    }
+}
